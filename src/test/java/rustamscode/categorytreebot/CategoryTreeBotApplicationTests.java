12ba2e334ff286cb44406360f86219c9dc6e8b79
AI_Telegram_Bot <rustamscode@gmail.com>
2024-10-22 17:22:00 +0400

    first commit

---
+++ src/test/java/rustamscode/categorytreebot/CategoryTreeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package rustamscode.categorytreebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryTreeBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
