8dda490054bfe6c14f32bf2e82f9dd6f71252fe6
AI_Telegram_Bot <rustamscode@gmail.com>
2024-10-22 22:59:23 +0400

    configuration was  done, telegram bot was configured

--- src/main/java/rustamscode/categorytreebot/CategoryTreeBotApplication.java
+++ src/main/java/rustamscode/categorytreebot/CategoryTreeBotApplication.java
@@ -2,8 +2,11 @@ package rustamscode.categorytreebot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import rustamscode.categorytreebot.telegram.TelegramProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(TelegramProperties.class)
 public class CategoryTreeBotApplication {
 
     public static void main(String[] args) {
